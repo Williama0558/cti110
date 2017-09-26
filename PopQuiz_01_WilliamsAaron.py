5e26f1a553bb5fdf049111e615f94879d2b74dbe
Williama0558 <31894283+Williama0558@users.noreply.github.com>
2017-09-26 10:55:12 -0400

    Add files via upload
    Finds the average of two numbers (9/26/17)

---
+++ PopQuiz_01_WilliamsAaron.py
@@ -0,0 +1,16 @@
+#CTI 110
+#Pop Quiz
+#Aaron Williams
+#09/26/17
+#This program will calculate the average of two numbers
+
+#Place where the user inputs their numbers
+firstNumber = float(input('Please type in the first number: '))
+secondNumber = float(input('Please type in the second number: '))
+
+#Calculations to find the average
+sumOfNumbers = firstNumber + secondNumber
+average = sumOfNumbers / 2
+
+#Displays the results
+print('The average of these two numbers will be:',average)
